9020cd8043ae72eaa9a5d038c091f5941767a517
WillyBoys <ethan.wilhelmsen9969@gmail.com>
2025-02-03 20:23:12 -0700

    Changed a ton of CSS stuff. Login looks pretty solid for now

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+ 
+ $(document).ready(function() {
+    $('.login-form').login(function(event) {
+        event.preventDefault(); // Prevent default form submission
+        window.location.href = "chat.html"; // Redirect to chat.html
+    });
+});
